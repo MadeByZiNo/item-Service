a0cff013c01c527f86e9f3fecd384cb7f41fe3b2
MadeByZiNo <38098073+MadeByZiNo@users.noreply.github.com>
2023-11-09 14:06:45 +0900

    상품등록 폼 및 상품등록처리(model Attribute)

--- src/main/java/hello/itemService/web/basic/BasicItemController.java
+++ src/main/java/hello/itemService/web/basic/BasicItemController.java
@@ -7,9 +7,7 @@ import jakarta.annotation.PostConstruct;
 import lombok.RequiredArgsConstructor;
 import org.springframework.stereotype.Controller;
 import org.springframework.ui.Model;
-import org.springframework.web.bind.annotation.GetMapping;
-import org.springframework.web.bind.annotation.PathVariable;
-import org.springframework.web.bind.annotation.RequestMapping;
+import org.springframework.web.bind.annotation.*;
 
 import java.util.List;
 
@@ -31,6 +29,48 @@ public class BasicItemController {
     public String item(@PathVariable long itemId, Model model){
         Item item = itemRepository.findById(itemId);
         model.addAttribute("item",item);
+        return "basic/item";
+    }
+
+    @GetMapping("/add")
+    public String addForm(){
+        return "basic/addForm";
+    }
+
+    //@PostMapping("/add")
+    public String addItemV1(@RequestParam String itemName,
+                       @RequestParam int price,
+                       @RequestParam Integer quantity,
+                       Model model) {
+
+        Item item = new Item();
+        item.setItemName(itemName);
+        item.setQuantity(quantity);
+        item.setPrice(price);
+
+        itemRepository.save(item);
+
+        model.addAttribute("item",item);
+
+        return "basic/item";
+    }
+
+    //@PostMapping("/add")
+    public String addItemV2(@ModelAttribute("item") Item item) {
+
+        itemRepository.save(item);
+        //model.addAttribute("item",item);  // 모델 에트리뷰트에서 자동 추가, 생략가능
+
+        return "basic/item";
+    }
+
+    @PostMapping("/add")
+    public String addItemV3(Item item) {
+
+        // 클래스명  Item -> item 이런식으로 모델에트리뷰트에 add해줌 ("item",item)
+        itemRepository.save(item);
+        //model.addAttribute("item",item);  // 모델 에트리뷰트에서 자동 추가, 생략가능
+
         return "basic/item";
     }
 
